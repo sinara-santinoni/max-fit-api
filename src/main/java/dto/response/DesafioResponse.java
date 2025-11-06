3391f4214944bc7c6febe0ea37d0cf73a23f176a
sinara_santinoni <sinara_santinoni@hotmail.com>
2025-11-06 15:03:10 -0300

    feat: Adiciona services de Usuario, Treino e Progresso com regras de negocio.

---
+++ src/main/java/dto/response/DesafioResponse.java
@@ -0,0 +1,23 @@
+package dto.response;
+
+import com.maxfit.model.StatusDesafio;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class DesafioResponse {
+    private Long id;
+    private Long alunoId;
+    private String nomeAluno;
+    private String titulo;
+    private String descricao;
+    private LocalDate dataInicio;
+    private LocalDate dataFim;
+    private StatusDesafio status;
+}
